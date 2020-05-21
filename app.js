3298e747a98b1524e2c808d7f92a0ae6faca117d
Darby Ross <darby.e.ross@gmail.com>
2020-05-20 20:29:16 -0500

    connect frontend to api
    use express-generator framework

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
-const express = require('express')
-const mysql = require('mysql')
-const app = express()
-const port = 3000
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mysql = require('mysql');
+const cors = require('cors');
 
-let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'personal_assistant'
-})
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api');
 
-connection.connect(function(err) {
-  if(err) {
-    return console.error('error: ', err.message);
-  }
-  console.log('Connected to MySQL');
-})
+const app = express();
 
-app.get('/', (req, res) => res.send('Hola Mundo!'))
+con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
 
-app.listen(port, () => console.log(`App listening at port ${port}`))
+con.connect(function(err) {
+  if (err) console.log(`Error: ${err}`);
+  console.log("Connected!");
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', apiRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
